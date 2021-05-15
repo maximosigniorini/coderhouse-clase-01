f157907db161a6d7163cf916c3d02732795b8e97
Maximo Signiorini <maximosigniorini97@gmail.com>
2021-05-14 22:01:56 -0300

    Segunda parte de la entrega

--- src/components/Buttons/ButtonCompra.jsx
+++ src/components/Buttons/ButtonCompra.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
-export default function ButtonCompra({ clase, color, texto, handlerClick }) {
+export default function ButtonCompra({ clase, color, texto, handlerClick, disabled }) {
 
     return (
         <button className='btnCompra'
             onClick={(handlerClick)}
+            disabled={disabled}
             style={
                 {
                     backgroundColor: color
--- src/components/container/ItemDetail/ItemCount.jsx
+++ src/components/container/ItemDetail/ItemCount.jsx
@@ -1,15 +1,21 @@
 import React, {useContext, useEffect, useState} from 'react' 
+import {NavLink} from 'react-router-dom'
 import { CartContext } from '../../../context/CartContext'
 import ButtonSignos from '../../Buttons/ButtonSignos'
 import ButtonCompra from '../../Buttons/ButtonCompra'
 
-export default function ItemCount(props){
+export default function ItemCount(props, btnActivo){
     
     const { addItems } = useContext(CartContext)
     const [cant, setCant] = useState(1)
+    const [botonActivo, setBotonActivo] = useState(true);
 
     useEffect(() => {
-
+        if(cant === 0){
+            setBotonActivo(true)
+        } else {
+            setBotonActivo(false)
+        }
     }, [cant])
 
     const sumar = () => {
@@ -19,11 +25,15 @@ export default function ItemCount(props){
     const restar = () => {
         if (cant > 0) {
             setCant(cant - 1)
+        } else if (cant === 0){
+            setCant(0)
+            setBotonActivo(true)
         }
     }
 
     const onAdd = () => {
         addItems(cant, props)
+        setBotonActivo(true)
     }
 
     return(
@@ -31,7 +41,8 @@ export default function ItemCount(props){
                     <ButtonSignos clase='fas fa-minus btn' handlerClick={restar} />
                     <span> {cant} </span>
                     <ButtonSignos clase='fas fa-plus btn' handlerClick={sumar} />
-                    <ButtonCompra color='green' texto='Comprar' handlerClick={onAdd} />
+                    <ButtonCompra color='green' texto='Comprar' handlerClick={onAdd} disabled={botonActivo}/>
+                    <NavLink to='/cart'><ButtonCompra color='violet' texto='Terminar Compra' disabled={!botonActivo}/></NavLink>
                 </div>
     )
 }
