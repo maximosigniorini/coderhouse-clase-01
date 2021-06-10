df29d5625294489d6db331abd3eabd1a65773a5e
Maximo Signiorini <maximosigniorini97@gmail.com>
2021-06-10 20:46:31 -0300

    Categorias estaticas

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -5,12 +5,11 @@ import ButtonSignos from '../Buttons/ButtonSignos'
 export default function Item(props) {
     return (
         <Link to={`/item/${props.id}`}>
-            <div className="item w-72 h-72 ml-16 bg-red-500 rounded-md">
+            <div className="w-72 h-64 ml-16 bg-red-500 rounded-md">
                 <h2 className='text-xl font-bold text-center mt-4 p-2'>{props.name}</h2>
                 <img src={props.img} alt='item' className='w-24 mx-auto'></img>
-                <p className='mt-2 ml-4 mt-4 w-48 italic inline-block'>{props.descripcion}</p>
-                <p className='font-bold inline-block float-right mr-4 mt-6 text-xl'>${props.precio}</p>
-                <ButtonSignos clase='fas fa-cart-plus mt-4 flex items-center justify-center' color='green' tamaño='2rem'></ButtonSignos>
+                <p className='font-bold inline-block float-right mr-16 mt-6 text-xl'>${props.precio}</p>
+                <ButtonSignos clase='fas fa-cart-plus mt-6 ml-16' color='green' tamaño='2rem'></ButtonSignos>
             </div>
         </Link>
     )
